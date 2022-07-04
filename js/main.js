502a2462ab6e2317363e4d56e1bf2be331e13a86
macs1756 <kindrativmaksum312@gmail.com>
2022-07-04 21:34:14 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+const btnBurger = document.querySelector(".header__burger");
+const contentBurger = document.querySelector(".burger__active");
+const language = document.querySelector(".nav__language");
+const login = document.querySelector(".login__list-wr");
+
+
+btnBurger.addEventListener("click", item =>{
+		btnBurger.classList.toggle("header__burger_close");
+		contentBurger.classList.toggle("burger__open");
+});
+
+window.addEventListener('click', function(e){
+	if(e.target.innerText === "EN"){
+		language.innerText = "RU";
+	}else if(e.target.innerText === "RU"){
+		language.innerText = "EN";
+	}
+	if(e.target.className === "nav__login-img" || e.target.className === "nav__login-span"){
+		login.classList.toggle("block");
+	}
+});
